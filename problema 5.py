21e5c801b2ed306bb6e6539b10cdb3ac1ebb067f
Cornelia Gherman <70709834+corneliag08@users.noreply.github.com>
2021-01-14 00:28:32 +0300

    Add files via upload

---
+++ problema 5.py
@@ -0,0 +1,7 @@
+with open('numar.txt','r') as f:
+    a=f.readline()
+for i in range(1,11):
+    t=str(i)+"*"+a+"="+str(i*int(a))
+    with open("inmultire.txt","a") as f:
+        f.write(x)
+        f.write(" ")
